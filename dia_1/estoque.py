0bc2f4cb01bcb1ccc29cf78a4e17060ea1332be0
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2025-12-13 15:45:20 -0300

    Evoluindo o projeto, dia 4

--- dia_1/estoque.py
+++ dia_1/estoque.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+from produto import Produto
+from lista_dados import produtos_de_mercado
 import time
+import random
+from faker import Faker
 # A classe é como a arquitura, a planta baixa da casa, que cada um dos nossos objetos que serão criados 
 # irão seguir, ele possuirão cada um deles um loca que será preenchido com informações próprias previamente 
 # específicadas na classe, chamados atributos, também cada objeto possuirá métodos próprios que manimulam 
@@ -43,11 +47,11 @@ class Estoque:
 
     
 
-    def adicionar_produto(self, nome= None):
+    def adicionar_produto(self, robo= None, nome= None, custo= None, lucro= None, imposto= None, quantidade= None, validade= None):
 
         # nome = input("Indique ") 
 
-        if not nome: 
+        if robo == False: 
 
             def adicionado_produtos():
 
@@ -55,14 +59,31 @@ class Estoque:
 
                 produtos_adicionados = 0 
                 while produtos_adicionados < quantidade_adicionar: 
+
                     nome = input("Indique o nome do produto: ")
+
+                    print()
+
+                    custo = float(input("Indique o custo do produto: "))
+
+                    print()
+                    lucro = float(input("indique a margende lucro: "))
+
+                    print()
+                    imposto = int(input("Indique a quantidade de imposto que será paga (em porcentagem): "))
+
+                    print()
+                    quantidade = int(input("Indique a quantidade de unidades que você irá adicionar deste produto: "))
+
+                    print()
+                    validade = int(input("Indique a quantidade de meses que esse produto ainda será válido: "))
 
                     # Futuramente irei criar uma classe produtos, 
                     # por enquanto estou apenas testando. 
 
-                    produto = nome
+                    produto_a = Produto(nome, custo, lucro, imposto, quantidade, validade)
 
-                    self.__produtos.append(produto)
+                    self.__produtos.append(produto_a)
 
                     time.sleep(1)
                     print("Produto adicionado com sucesso!")
@@ -86,21 +107,32 @@ class Estoque:
                 adicionado_produtos()
 
 
-estoque_1 = Estoque("Calçados")
+        else: 
 
-print("Tipo do Estoque:", estoque_1.tipo)
-print("Produtos:", estoque_1.produtos)
-print("Data de Criação do Estoque:", estoque_1.data_criacao) 
+            produto_a = Produto(nome, custo,lucro, imposto, quantidade, validade)
 
-estoque_1.adicionar_produto()
+            self.__produtos.append(produto_a)
+
+
+
+
+
+
+# estoque_1 = Estoque("Calçados")
+
+# print("Tipo do Estoque:", estoque_1.tipo)
+# print("Produtos:", estoque_1.produtos)
+# print("Data de Criação do Estoque:", estoque_1.data_criacao) 
+
+# estoque_1.adicionar_produto()
 
 # print("Produtos: " + str(estoque_1.produtos))
 
-lista_produtos = estoque_1.produtos
+# lista_produtos = estoque_1.produtos
 
-print("Produtos Criados: ")
-for produto in lista_produtos: 
-    print(produto)
+# print("Produtos Criados: ")
+# for produto in lista_produtos: 
+#     print(produto)
 
 
 # # tempo_agora = datetime.timestamp(1)
@@ -108,3 +140,52 @@ for produto in lista_produtos:
 
 # # print("Tempo agora: ", tempo_agora)
 # print("Tempo: ", tempo)
+
+
+estoque_1 = Estoque("Supermecado.")
+
+# estoque_1.adicionar_produto()
+
+
+# for produto in estoque_1.produtos:
+
+#     print(produto.nome)
+
+faker = Faker('pt_BR')
+
+def criar_produtos(produtos_nomes):
+
+
+
+    for numero in range(0, 100): 
+
+        random.shuffle(produtos_nomes)
+        nome = random.choice(produtos_nomes)
+        marca = faker.name_male()
+        custo = random.uniform(0.99, 15.95)
+
+
+        lucro = random.randint(10, 50)
+        imposto = random.randint(20, 30)
+        quantidade = random.randint(10, 200)
+        validade = random.randint(30, 120)
+
+
+        estoque_1.adicionar_produto(True, nome, custo, lucro, imposto, quantidade, validade)
+
+
+def mostrar_produtos(objeto_estoque):
+
+    produtos_mostrados = 0
+    indice = 0
+    while produtos_mostrados < len(objeto_estoque.produtos):
+
+        print(f"""Nome: {objeto_estoque.produtos[indice].nome}""")
+
+        indice += 1 
+        produtos_mostrados += 1
+
+
+criar_produtos(produtos_de_mercado)
+
+mostrar_produtos(estoque_1)
--- dia_1/produto.py
+++ dia_1/produto.py
@@ -3,17 +3,18 @@ from datetime import datetime, timedelta
 
 class Produto:
 
-    def __init__(self, nome, preco, custo, quantidade, validade):
+    def __init__(self, nome, custo, lucro, imposto, quantidade, validade):
 
         # Privando os atributos para que eles não sejam acessados de forma pública. 
 
         self.__nome = nome 
-        self.__preco = preco 
         self.__custo = custo 
+        self.__lucro = lucro
+        self.__imposto = imposto
         self.__quantidade = quantidade
-        self.__imposto = 0
         self.__validade = datetime.now() + timedelta(days= validade) 
         self.__data_criacao = datetime.now()
+
 
     # Definindo a regra de leitura, no caso por enquanto teremos uma regra de leitura 
     # simples bem parecida com a leitura de um atributo público. 
@@ -26,31 +27,37 @@ class Produto:
         return self.__nome 
 
     @property 
-    def preco(self):
+    def custo(self):
 
         # Utilizamos o self para acessar os atributos do próprio objeto, bem parecido com o
         # que fazemos com objetos que já foram criados. 
 
-        return self.__preco 
+        return self.__custo
 
     @property 
-    def custo(self):
-        return self.__custo 
+    def lucro(self):
+        return self.__lucro
 
     @property
+    def quantidade(self):
+        return self.__quantidade
+
+    @property 
     def imposto(self):
-        return self.__imposto 
+
+        # senha = int(input("Digite a sua senha: "))
+
+        # if senha == 1234:
+        #     return self.__validade 
+
+        # else:
+        #     return "Acesso negado!"
+
+        return self.__imposto
 
     @property 
     def validade(self):
-
-        senha = int(input("Digite a sua senha: "))
-
-        if senha == 1234:
-            return self.__validade 
-
-        else:
-            return "Acesso negado!"
+        return self.__validade 
 
 
     @property
@@ -66,33 +73,43 @@ class Produto:
         self.__nome = novo_nome
 
 
-    @preco.setter 
-    def preco(self, novo_preco):
-        self.__preco = novo_preco 
-
-
     @custo.setter 
     def custo(self, novo_custo):
         self.__custo = novo_custo 
 
 
+    @custo.setter 
+    def lucro(self, novo_lucro):
+        self.__lucro = novo_lucro
+
+
+    @quantidade.setter 
+    def quantidade(self, nova_quantidade):
+        self.__quantidade = nova_quantidade 
+
+
     @imposto.setter 
     def imposto(self, novo_imposto):
-        self.__imposto = novo_imposto 
+        self.__imposto = novo_imposto
 
 
-    
+    @validade.setter
+    def validade(self, nova_validade):
+        self.__validade 
+
+
+    @data_criacao.setter 
+    def data_criacao(self, nova_data):
+        self.__data_criacao = nova_data
 
 
 
+# produto_1 = Produto("Refrigerante", 10.99, 5, 10)
 
 
-produto_1 = Produto("Refrigerante", 10.99, 5, 10)
+# validade_do_produto = produto_1.validade
 
-
-validade_do_produto = produto_1.validade
-
-print(validade_do_produto) 
+# print(validade_do_produto) 
 
 # print(produto_1.)
 
